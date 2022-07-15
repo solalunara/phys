6ebca9944b06f86877a73692082a13879724b64f
solregem <greenberg04@gmail.com>
2022-07-15 22:29:49 +0000

    update mkclass.sh

--- mkclass.sh
+++ mkclass.sh
@@ -3,6 +3,7 @@
 for class in "$@"
 do
 	class_capital=$(echo "$class" | tr 'a-z' 'A-Z')
-	echo -e "#ifndef "$class_capital"_H\n#define "$class_capital"_H\n\n#pragma once\n\nstruct "$class"\n{\n\n};\n\n#endif" > header/"$class".h
+	class_capital_first=$(echo "${class:0:1}" | tr 'a-z' 'A-Z')$(echo "${class:1}")
+	echo -e "#ifndef "$class_capital"_H\n#define "$class_capital"_H\n\n#pragma once\n\nstruct "$class_capital_first"\n{\n\n};\n\n#endif" > header/"$class".h
 	echo -e "#include \""$class".h\"\n" > source/"$class".cpp
 done
