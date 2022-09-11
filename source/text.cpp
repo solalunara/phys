5402935e0b63efcdad86ca3d53681f806ad90015
solregem <greenberg04@gmail.com>
2022-09-11 19:30:54 +0000

    make text rendering centred

--- source/text.cpp
+++ source/text.cpp
@@ -6,8 +6,21 @@
 UIText::UIText( const char *text, float x, float y, float scale, vec3 color, Window *container ) :
     UIElement( container, Transform( vec3( x, y, -1 ), glm::identity<quat>(), glm::one<vec3>() ) )
 {
+    unsigned long long len = strlen( text );
+    struct Data
+    {
+        Character ch;
+        float xpos;
+        float ypos;
+        float w;
+        float h;
+    };
+    Data data[ len ] {};
+
+    float TextWidth = 0;
+
     float xnet = 0.f;
-    for ( int i = 0; i < strlen( text ); ++i )
+    for ( unsigned long long i = 0; i < len; ++i )
     {
         Character ch = Characters[ text[ i ] ];
 
@@ -15,20 +28,41 @@ UIText::UIText( const char *text, float x, float y, float scale, vec3 color, Win
         float ypos = (ch.Size.y - ch.Bearing.y) * scale;
 
         float w = ch.Size.x * scale;
-        xnet += w;
         float h = ch.Size.y * scale;
 
-        Meshes.push_back( new CharacterMesh( vec2( -w / 2, -h / 2 ), vec2( w / 2, h / 2 ),
-            ch.TextureID->FindLocalTexture( container ), Transform( vec3( xpos, ypos, 0 ), glm::identity<quat>(), glm::one<vec3>() ), container, color, text[ i ], this ) );
+        data[ i ] = Data{ ch, xpos, ypos, w, h };
 
         // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+        if ( i + 1 < len )
+            xnet += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+
+    TextWidth = xnet;
+
+    for ( unsigned long long i = 0; i < len; ++i )
+    {
+        Meshes.push_back( new CharacterMesh( vec2( -data[ i ].w / 2, -data[ i ].h / 2 ), vec2( data[ i ].w / 2, data[ i ].h / 2 ),
+            data[ i ].ch.TextureID->FindLocalTexture( container ), Transform( vec3( data[ i ].xpos - TextWidth / 2, data[ i ].ypos, 0 ), glm::identity<quat>(), glm::one<vec3>() ), 
+            container, color, text[ i ], this ) );
     }
 }
 
 GameText::GameText( const char *text, float x, float y, float z, float scale, vec3 color, Window *container ) :
     GameElement( container, Transform( vec3( x, y, z ), glm::identity<quat>(), glm::one<vec3>() ) )
 {
+    unsigned long long len = strlen( text );
+    struct Data
+    {
+        Character ch;
+        float xpos;
+        float ypos;
+        float w;
+        float h;
+    };
+    Data data[ len ] {};
+
+    float TextWidth = 0;
+
     float xnet = 0.f;
     for ( int i = 0; i < strlen( text ); ++i )
     {
@@ -38,13 +72,19 @@ GameText::GameText( const char *text, float x, float y, float z, float scale, ve
         float ypos = (ch.Size.y - ch.Bearing.y) * scale;
 
         float w = ch.Size.x * scale;
-        xnet += w;
         float h = ch.Size.y * scale;
 
-        Meshes.push_back( new CharacterMesh( vec2( -w / 2, -h / 2 ), vec2( w / 2, h / 2 ),
-            ch.TextureID->FindLocalTexture( container ), Transform( vec3( xpos, ypos, 0 ), glm::identity<quat>(), glm::one<vec3>() ), container, color, text[ i ], this ) );
-
         // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
-        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+        if ( i + 1 < len )
+            xnet += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+    }
+
+    TextWidth = xnet;
+
+    for ( unsigned long long i = 0; i < len; ++i )
+    {
+        Meshes.push_back( new CharacterMesh( vec2( -data[ i ].w / 2, -data[ i ].h / 2 ), vec2( data[ i ].w / 2, data[ i ].h / 2 ),
+            data[ i ].ch.TextureID->FindLocalTexture( container ), Transform( vec3( data[ i ].xpos - TextWidth / 2, data[ i ].ypos, 0 ), glm::identity<quat>(), glm::one<vec3>() ), 
+            container, color, text[ i ], this ) );
     }
 }
