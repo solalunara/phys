75c73ddf3a208b1a5140d07d336863b5857509bb
solregem <greenberg04@gmail.com>
2024-01-21 00:10:27 +0000

    differential func

--- header/function.h
+++ header/function.h
@@ -68,7 +68,7 @@ struct DifferentialFunction :
     public GameElement
 {
     DifferentialFunction( float min, float max, function<vec3( float )> InitialState, function<vec3( float, float, float, float )> fn, Transform *transform, GlobalTexture *tex, Window *w, float step = 0.03f ) :
-        GameElement( w, transform ), PreviousState( InitialState ), fn( fn ), min( min ), max( max ), step( step )
+        GameElement( w, transform ), PreviousState( InitialState ), fn( fn ), min( min ), max( max ), step( step ), LastReset( DynamicFunction::FunctionTime )
     {
         u_long n_Segments = (u_long)( ( max - min ) / step ) + 1;
         for ( u_long i = 0; i < n_Segments; ++i )
@@ -86,22 +86,67 @@ struct DifferentialFunction :
     virtual void Render()
     {
         PreviousStateSave = new vec3[ Elements.size() ];
+
+        bool smoothed = false;
         for ( u_long i = 0; i < Elements.size(); ++i )
         {
-            PreviousStateSave[ i ] = Elements[ i ]->transform->pos;
+            //smooth the function out every so often, to prevent drift
+            if ( DynamicFunction::FunctionTime - LastReset >= 5.f 
+                && i >= 2 
+                && i < Elements.size() - 2 )
+            {
+                smoothed = true;
+                PreviousStateSave[ i ] = ( Elements[ i - 2 ]->transform->pos + 
+                                            Elements[ i - 1 ]->transform->pos + 
+                                            Elements[ i ]->transform->pos + 
+                                            Elements[ i + 1 ]->transform->pos + 
+                                            Elements[ i + 2 ]->transform->pos ) / 5.f;
+            }
+            else
+                PreviousStateSave[ i ] = Elements[ i ]->transform->pos;
+
             PreviousStateSave[ i ].x = i * step + min;
         }
-        PreviousState = [ this ] ( float x ) { if ( x >= min && x <= max ) return PreviousStateSave[ (u_long)( ( x - min ) / step ) ]; else return vec3( x, 0, 0 ); };
+
+        if ( smoothed )
+            LastReset = DynamicFunction::FunctionTime;
+
+        PreviousState = [ this ] ( float x ) { if ( x >= min && x <= max ) return PreviousStateSave[ (u_long)round( ( x - min ) / step ) ]; else 
+            {
+                //Calculate derivatives and use taylor series to interpolate
+                if ( x < min )
+                {
+                    float dx = min - x;
+                    vec3 ddx = ( PreviousState( x + 2 * dx ) - PreviousState( x + dx ) ) / ( dx );
+                    vec3 d2xdx2 = 1 / ( dx * dx ) * ( PreviousState( x + dx ) - 2.f * PreviousState( x + 2 * dx ) + PreviousState( x + 3 * dx ) );
+                    vec3 pos = PreviousState( x + dx ) + ddx * dx + ( 1.f / 2 ) * d2xdx2 * ( dx * dx );
+                    pos.x = x;
+                    return pos;
+                }
+                else
+                {
+                    float dx = x - max;
+                    vec3 ddx = -( PreviousState( x - 2 * dx ) - PreviousState( x - dx ) ) / ( dx );
+                    vec3 d2xdx2 = -1 / ( dx * dx ) * ( PreviousState( x - dx ) - 2.f * PreviousState( x - 2 * dx ) + PreviousState( x - 3 * dx ) );
+                    vec3 pos = PreviousState( x - dx ) + ddx * dx + ( 1.f / 2 ) * d2xdx2 * ( dx * dx );
+                    pos.x = x;
+                    return pos;
+                }
+            }
+        };
 
         for ( u_long i = 0; i < Elements.size(); ++i )
             Elements[ i ]->transform->pos = fn( i * step + min, step, DynamicFunction::FunctionTime, FunctionDeltaTime );
         Element::Render();
+
+        delete[] PreviousStateSave;
     }
 
     function<vec3( float )> PreviousState;
     vec3 *PreviousStateSave;
     function<vec3( float, float, float, float )> fn;
     float min, max, step;
+    float LastReset;
 
     static inline float FunctionDeltaTime;
 };
