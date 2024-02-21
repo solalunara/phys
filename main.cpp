c40645fec29242611819b8db71db3dd538feabd0
solregem <greenberg04@gmail.com>
2024-02-21 14:04:23 +0000

    dying wish

--- main.cpp
+++ main.cpp
@@ -140,6 +140,16 @@ int CameraMovement( Window *window, float dt, map<Setting, int> Settings )
     if ( window->GetKeyFlag( GLFW_KEY_E ) )
         window->CameraTransform.pos += window->CameraTransform.LocalToWorldDirection( glm::vec3( 0, -dt * 3, 0 ) );
 
+/*
+    if ( window->GetKeyFlag( GLFW_KEY_SPACE ) )
+    {
+        window->SetKeyFlag( GLFW_KEY_SPACE, false );
+
+        DifferentialFunction::FunctionDeltaTime = 1 / 60.f;
+        DynamicFunction::FunctionTime += 1 / 60.f;
+    }
+    */
+
 
     //can't do *= because it does the * in the wrong order (rot * new instaed of new * rot)
     if ( window->LockCursor )
@@ -223,10 +233,10 @@ int main( int argc, const char *argv[] )
             c->collide = new AABB( *c, glm::vec3( -.5f ), glm::vec3( .5f ) );
         }
     
-    Cube *PhysCube = new Cube( vec3( -.5f ), vec3( .5f ), new Transform( vec3( 0, 5, 0 ), glm::identity<quat>(), glm::one<vec3>() ), Textures[ InbuiltTexture::universe ], main );
-    PhysCube->transform->rot = glm::angleAxis( glm::radians( 45.f ), glm::vec3( 1, 0, 0 ) );
-    PhysCube->phys_obj = new PhysicsObject( *PhysCube, 1 );
-    PhysCube->collide = new Collide( *PhysCube, glm::vec3( -1.f ), glm::vec3( 1.f ) );
+    //Cube *PhysCube = new Cube( vec3( -.5f ), vec3( .5f ), new Transform( vec3( 0, 5, 0 ), glm::identity<quat>(), glm::one<vec3>() ), Textures[ InbuiltTexture::universe ], main );
+    //PhysCube->transform->rot = glm::angleAxis( glm::radians( 45.f ), glm::vec3( 1, 0, 0 ) );
+    //PhysCube->phys_obj = new PhysicsObject( *PhysCube, 1 );
+    //PhysCube->collide = new Collide( *PhysCube, glm::vec3( -1.f ), glm::vec3( 1.f ) );
 
     //Cube *PhysCube2 = new Cube( vec3( -.5f ), vec3( .5f ), new Transform( vec3( 0, 10, 0 ), glm::identity<quat>(), glm::one<vec3>() ), Textures[ InbuiltTexture::universe ], main );
     //PhysCube2->transform->rot = glm::angleAxis( glm::radians( 30.f ), glm::vec3( 1, 0, 0 ) );
@@ -235,36 +245,33 @@ int main( int argc, const char *argv[] )
 
 
     // fun with differential equations
-    /*
-    #define MGT 30.f
+    
     glm::quat i = glm::angleAxis( glm::radians( 90.f ), vec3( 1, 0, 0 ) );
-    Axis *x = new Axis( i_hat, vec3( 0, 0, 0 ), black, 10, 1, main );
-    Axis *y = new Axis( j_hat, vec3( 0, 0, 0 ), black, 10, 1, main );
-    Axis *z = new Axis( k_hat, vec3( 0, 0, 0 ), black, 10, 1, main );
-    float wavelength = 2.f;
-    float frequency = 1.f;
-    float k = 2.f * M_PI / wavelength;
-    float w = frequency * ( 2.f * M_PI );
+    Axis *x = new Axis( i_hat, vec3( 0, 0, 0 ), Textures[ InbuiltTexture::black ], 10, 1, main );
+    Axis *y = new Axis( j_hat, vec3( 0, 0, 0 ), Textures[ InbuiltTexture::black ], 10, 1, main );
+    Axis *z = new Axis( k_hat, vec3( 0, 0, 0 ), Textures[ InbuiltTexture::black ], 10, 1, main );
+    float p = 1;
+    float m = 1;
+    float hbar = 1;
     Transform *DefTransform = new Transform( glm::zero<vec3>(), glm::identity<quat>(), glm::one<vec3>() );
-    DifferentialFunction *quantum = new DifferentialFunction( -10, 10, [] ( float x ) { return vec3( x, sin( 10 * x ) * exp( -( x * x ) ), cos( 10 * x ) * exp( -( x * x ) ) ); }, [ &quantum, i ] ( float x, float dx, float t, float dt ) {
-        if ( x < quantum->min || x > quantum->max ) return vec3( x, 0, 0 );
-        if ( dt <= 0 ) return quantum->PreviousState( x );
-        vec3 prev = quantum->PreviousState( x );
-        vec3 d2xdx2 = ( quantum->PreviousState( x + MGT * dx ) + quantum->PreviousState( x - MGT * dx ) - 2.f * prev ) / ( MGT * dx * MGT * dx );
-        d2xdx2.x = 0;
-        return -( i * d2xdx2 ) * dt / 100.f + prev;
-    }, DefTransform, black, main, 0.01f );
-    */
+    DifferentialFunction *quantum = new DifferentialFunction( -3, 3, [] ( float x ) { return vec2( exp( -( x * x ) ), 0 ); }, 
+        // pf/pt = F[ f, pf/px, p2f/px2, x, t ]
+        [ p, m, hbar ] ( vec2 f, vec2 pfpx, vec2 p2fpx2, float x, float t ) {
+        vec2 val = p * p / 2 / m / hbar * p2fpx2 + x * x / hbar * f;
+        return vec2( val.y, -val.x ) / 1000.f;
+        //return vec2( 0, 0 );
+    }, DefTransform, Textures[ InbuiltTexture::black ], main, 0.01f );
 
     main->CameraTransform.pos = vec3( 1, 0, 3 );
 
     vec3 player_mins = vec3( -.2f, -1.2f, -.2f );
     vec3 player_maxs = vec3(  .2f,   .2f,  .2f );
-    main->PlayerPhysics = new PhysicsBaseObject( 10.f, player_mins, player_maxs );
+    //main->PlayerPhysics = new PhysicsBaseObject( 10.f, player_mins, player_maxs );
 
     while ( true )
     {
         //per frame for all windows
+
         double t_new = glfwGetTime();
         dt = t_new - t;
         t = t_new;
@@ -298,12 +305,12 @@ int main( int argc, const char *argv[] )
                             {
                                 float MassFraction = Windows[ i ]->PlayerPhysics->mass / other->object.phys_obj->mass;
 
-                                Windows[ i ]->CameraTransform.pos += MassFraction * data.Penetration * data.Normal;
-                                other->object.transform->SetAbsOrigin( other->object.transform->GetAbsOrigin() + ( 1 / MassFraction ) * data.Penetration * data.Normal );
+                                //Windows[ i ]->CameraTransform.pos += MassFraction * data.Penetration * data.Normal;
+                                //other->object.transform->SetAbsOrigin( other->object.transform->GetAbsOrigin() + ( 1 / MassFraction ) * data.Penetration * data.Normal );
                             }
                             else
                             {
-                                Windows[ i ]->CameraTransform.pos += data.Penetration * data.Normal;
+                                //Windows[ i ]->CameraTransform.pos += data.Penetration * data.Normal;
                                 Windows[ i ]->PlayerPhysics->ZeroMomentumIntoPlane( data.Normal );
                             }
                         }
@@ -322,9 +329,25 @@ int main( int argc, const char *argv[] )
                 }
             }
 
+
+            StaticFunction *approx_0 = NULL;
+            StaticFunction *approx_1 = NULL;
+            if ( quantum->PreviousStateSave )
+            {
+                approx_0 = quantum->FourierFnApprox( quantum->FourierApproximation( 100 ), -5, 5, Textures[ InbuiltTexture::universe ], main );
+                approx_1 = quantum->FourierFnApprox( quantum->FourierDerivative( quantum->FourierDerivative( quantum->FourierApproximation( 100 ) ) ), -5, 5, Textures[ InbuiltTexture::universe ], main );
+            }
+
             WindowRender( Windows[ i ] );
+
+            if ( approx_0 )
+                delete approx_0;
+            if ( approx_1 )
+                delete approx_1;
 
             WindowPostFrame( Windows[ i ] );
         }
+        if ( DifferentialFunction::FunctionDeltaTime != 0 )
+            DifferentialFunction::FunctionDeltaTime = 0;
     }
 }
--- source/function.cpp
+++ source/function.cpp
@@ -1,1 +1,65 @@
 #include "function.h"
+#include <string>
+
+DifferentialFunction::FourierConstants DifferentialFunction::FourierDerivative( FourierConstants f )
+{
+    FourierConstants z = FourierConstants();
+    z.N = f.N;
+    z.T = f.T;
+    z.a_n = vector<vec2>( z.N );
+    z.b_n = vector<vec2>( z.N );
+
+    //first derivative
+    // b_n = -a_n * 2pi n / T
+    // a_n =  b_n * 2pi n / T
+    for ( int j = 0; j < z.N; ++j )
+    {
+        z.b_n[ j ] = -f.a_n[ j ] * 2.f * glm::pi<float>() * (float)j / z.T;
+        z.a_n[ j ] =  f.b_n[ j ] * 2.f * glm::pi<float>() * (float)j / z.T;
+    }
+    return z;
+}
+
+DifferentialFunction::FourierConstants DifferentialFunction::FourierApproximation( uint N )
+{
+    DifferentialFunction::FourierConstants f = FourierConstants();
+    f.N = N;
+    f.T = max - min;
+
+    f.a_n = vector<vec2>( f.N );
+    f.b_n = vector<vec2>( f.N );
+
+    //a_n = 2/T int -T/2 T/2 of f(x) cos[ 2 pi n / T * x ] dx
+    //a_n = 2/T sum 0 to N of f_i cos[ 2 pi n / T * (min+i*step)] * step
+    for ( int n = 0; n < N; ++n )
+    {
+        vec2 an = glm::zero<vec2>();
+        for ( int i = 0; i < Elements.size(); ++i )
+        {
+            vec3 an3 = PreviousStateSave[ i ] * glm::cos( 2.f * glm::pi<float>() * n / f.T * (min+i*step) );
+            an += vec2( an3.y, an3.z ) * step * 2.f / f.T;
+        }
+        f.a_n[ n ] = an;
+
+        vec2 bn = glm::zero<vec2>();
+        for ( int i = 0; i < Elements.size(); ++i )
+        {
+            vec3 bn3 = PreviousStateSave[ i ] * glm::sin( 2.f * glm::pi<float>() * n / f.T * (min+i*step) );
+            bn += vec2( bn3.y, bn3.z ) * step * 2.f / f.T;
+        }
+        f.b_n[ n ] = bn;
+    }
+
+    return f;
+}
+
+StaticFunction *DifferentialFunction::FourierFnApprox( FourierConstants f, float min, float max, GlobalTexture *texture, Window *window )
+{
+    auto FourierSeries = [ f ] ( float x ) {
+        vec2 val = f.a_n[ 0 ] / 2.f;
+        for ( int n = 1; n < f.N; ++n )
+            val += f.a_n[ n ] * glm::cos( 2.f * glm::pi<float>() * n / f.T * x ) + f.b_n[ n ] * glm::sin( 2.f * glm::pi<float>() * n / f.T * x );
+        return val;
+    };
+    return new StaticFunction( min, max, FourierSeries, new Transform( glm::zero<vec3>(), glm::identity<quat>(), glm::one<vec3>() ), texture, window );
+}
